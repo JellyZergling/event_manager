02ea6542587bbdcba0ac850d33eb36f2699d6e44
JellyZergling <77082540+JellyZergling@users.noreply.github.com>
2023-08-14 18:09:18 +0900

    commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app id="appbar">
+      <v-container>
+        <v-row class="fill-height">
+          <v-col cols="auto" class="d-flex align-center">
+            <img src="./components/sample_logo1.png" id="appbar-logo" />
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="auto" class="d-flex align-center justify-center">
+            <v-btn
+              v-for="link in links"
+              :key="link"
+              :text="link"
+              variant="text"
+            ></v-btn>
+          </v-col>
+          <v-col cols="auto" class="d-flex align-center"> </v-col>
+        </v-row>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                  :title="`List Item ${n}`"
+                ></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  color="grey-lighten-4"
+                  link
+                  title="Refresh"
+                ></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg"> </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+const links = ['Main', 'Messages', 'Profile', 'Updates'];
+</script>
+
+<script>
+export default {
+  data: () => ({
+    links: ['Main', 'Messages', 'Profile', 'Updates'],
+  }),
+};
+</script>
